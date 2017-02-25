6d711be33b8791d4264603d3723dc04d1ca3a0f2
RS <roboszyn@gmail.com>
2017-02-25 04:35:26 +0000

    Exercise 8.5

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+var a = prompt('Podaj dowolną liczbę');
+var b = prompt('Podaj drugą dowolną liczbę');
+
+var value = (a*a) + (2 * a * b) - (b*b);
+
+console.log(value);
+
+if (value > 0) {console.log('Wynik dodatni');}
+else if (value < 0) {console.log('Wynik ujemny');}
+else if (value == 0) {console.log('Wynik jest równy zero');}
