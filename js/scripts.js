bfd6f29ddbcc5e8f4fb69cda6f1c1860b05a3092
RS <roboszyn@gmail.com>
2017-02-27 00:51:06 +0000

    amending condition in if

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,10 @@ var value = (a*a) + (2 * a * b) - (b*b);
 
 console.log(value);
 
-if (value > 0) {console.log('Wynik dodatni');}
-else if (value < 0) {console.log('Wynik ujemny');}
-else if (value == 0) {console.log('Wynik jest równy zero');}
+if (value > 0) {
+	console.log('Wynik dodatni');
+} else if (value < 0) {
+	console.log('Wynik ujemny');
+} else {
+	console.log('Wynik jest równy zero');
+}
